b01fd81658024ee4d92e0cd1782cc6cfd17130d9
Pavel Kuts <pavel.kuts@gmail.com>
2021-01-28 23:40:10 +0200

    5, 6 + css

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+console.log(inputRef, outputRef);
+
+inputRef.addEventListener('blur', event => {
+    // const value = event.target.value; 
+    const { value } = event.target; 
+    value.length === 0 ? outputRef.textContent = "незнакомец" : outputRef.textContent = value;
+})
--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const inputRef = document.querySelector('#validation-input');
+const inputLength = inputRef.dataset.length; 
+
+inputRef.addEventListener('blur', e => {
+    const value = e.target.value;
+   
+    if(value.length == inputLength) {
+        inputRef.classList.remove('invalid');
+        inputRef.classList.add('valid');
+    }
+    else {
+        inputRef.classList.remove('valid');
+        inputRef.classList.add('invalid');
+    };
+})
