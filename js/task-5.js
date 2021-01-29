788c937571c0ae0b4f0bf0c8fbc5558b24f06879
Pavel Kuts <pavel.kuts@gmail.com>
2021-01-29 20:45:18 +0200

    5

--- js/task-5.js
+++ js/task-5.js
@@ -4,7 +4,7 @@ const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 console.log(inputRef, outputRef);
 
-inputRef.addEventListener('blur', event => {
+inputRef.addEventListener('input', event => {
     // const value = event.target.value; 
     const { value } = event.target; 
     value.length === 0 ? outputRef.textContent = "незнакомец" : outputRef.textContent = value;
